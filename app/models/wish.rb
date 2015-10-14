78ca3fdf07e20b095877b2bd3419e843836aade5
drzel <shayolden@hotmail.com>
2015-10-14 23:17:36 +1100

    make locations dependent on wishes

--- app/models/wish.rb
+++ app/models/wish.rb
@@ -1,7 +1,7 @@
 class Wish < ActiveRecord::Base
   belongs_to :user
   acts_as_votable
-  has_one :location
+  has_one :location, :dependent => :destroy
 
   def score
     self.get_upvotes.size - self.get_downvotes.size
