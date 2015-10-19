71a00abc9c1d408bb3571b8c6e2c3328cecdc2d8
drzel <shayolden@hotmail.com>
2015-10-19 14:28:04 +1100

    correct authenticated wish actions

--- app/controllers/wishes_controller.rb
+++ app/controllers/wishes_controller.rb
@@ -1,6 +1,6 @@
 class WishesController < ApplicationController
   before_action :set_wish, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, only: [:show, :edit, :update, :destroy, :upvote, :downvote]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy, :upvote, :downvote]
 
   # GET /wishes
   # GET /wishes.json
